81ffbaaaac77e632ad8a5859a7f6fbfd67fc93e4
louis.j <rokmctkd6@gmail.com>
2020-08-10 11:25:57 +0900

    Initial commit

---
+++ routes/middlewares.js
@@ -0,0 +1,56 @@
+const jwt = require('jsonwebtoken');
+const RateLimit = require('express-rate-limit');
+
+exports.isLoggedIn = (req, res, next) => {
+    if (req.isAuthenticated()) {
+        next();
+    } else {
+        res.status(403).send('로그인 필요');
+    }
+};
+
+exports.isNotLoggedIn = (req, res, next) => {
+    if (!req.isAuthenticated()) {
+        next();
+    } else {
+        res.redirect('/');
+    }
+};
+
+exports.verifyToken = (req, res, next) => {
+    try {
+        req.decoded = jwt.verify(req.headers.authorization, process.env.JWT_SECRET);
+        return next();
+    } catch (error){
+        if (error.name === 'TokenExpiredError'){
+            return res.status(419).json({
+                code:419,
+                message: ' Expired tocken'
+            });
+        }
+        return res.status(401).json({
+            code : 401,
+            message : 'invalid Token'
+        });
+    }
+}
+
+exports.apiLimiter = new RateLimit({
+    windowMs: 60 * 1000,
+    max: 1,
+    delayMs: 0,
+    handler(req,res){
+        res.status(this.statusCode).json({
+            code: this.statusCode,
+            message: 'request only one time per one min',
+        });
+    },
+
+});
+
+exports.deprecated = (req, res) => {
+    res.status(410).json({
+        code: 410,
+        message: "please update the version.",
+    });
+};
